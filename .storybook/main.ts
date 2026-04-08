a50c0e2a5bc5af86f968eacf83a399bcf037df0f
Greg N Black <orekhovgn@gmail.com>
2026-04-08 03:02:52 +0200

    fix: storybook automigrate

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,25 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: [
     '../packages/components/**/stories/*.stories.@(ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-essentials',
-  ],
+  addons: [getAbsolutePath("@storybook/addon-essentials"), "@chromatic-com/storybook"],
   framework: {
-    name: '@storybook/angular',
+    name: getAbsolutePath("@storybook/angular"),
     options: {},
   },
-  docs: {},
+  docs: {
+    autodocs: true
+  },
   staticDirs: [
     { from: '../packages/core/styles', to: '/styles' },
   ],
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
